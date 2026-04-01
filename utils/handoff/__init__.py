114ee91aef8ea17544aea8a9ca54899ce00ee7ff
RKChoi <152466815+rikychoi@users.noreply.github.com>
2026-04-01 17:20:06 +0900

    fix: remove unused imports

--- utils/handoff/__init__.py
+++ utils/handoff/__init__.py
@@ -1,5 +1,4 @@
-from .dto import build_crawl_event
 from .sender import send_event, send_event_from_env
 
-__all__ = ["build_crawl_event", "send_event", "send_event_from_env"]
+__all__ = ["send_event", "send_event_from_env"]
 
